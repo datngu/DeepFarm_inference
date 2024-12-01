ce90784b290a5e3cc4faf8412dcead11aefc666b
Dat T Nguyen <n.dat@outlook.com>
2024-12-01 11:05:08 +0100

    1st

--- run_cattle_rexld.sh
+++ run_cattle_rexld.sh
@@ -17,16 +17,16 @@ export NXF_SINGULARITY_CACHEDIR=/mnt/users/ngda/sofware/singularity
 export TOWER_ACCESS_TOKEN=eyJ0aWQiOiA3OTAxfS4xNGY5NTFmOWNiZmEwNjZhOGFkYzliZTg3MDc4YWI4ZTRiYTk4ZmI5
 
 
-SPEC='pig'
+SPEC='cattle'
 
+vcf_dir=/mnt/ScratchProjects/Aqua-Faang/dat_projects/vip_nf/data_vcf
 model_dir="/mnt/ScratchProjects/Aqua-Faang/dat_projects/dl_project/analysis_paper/selected_models"
 model="${model_dir}/${SPEC}_DanQ.h5"
 
-genome='/mnt/users/ngda/genomes/pig/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa'
-# genome='/mnt/users/ngda/genomes/cattle/Bos_taurus.ARS-UCD1.2.dna_sm.toplevel.fa'
+# genome='/mnt/users/ngda/genomes/pig/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa'
+genome='/mnt/users/ngda/genomes/cattle/Bos_taurus.ARS-UCD1.2.dna_sm.toplevel.fa'
 # genome='/mnt/users/ngda/genomes/chicken/chromosomes_1_to_28_and_MT.fa'
 # genome='/mnt/users/ngda/genomes/atlantic_salmon/Salmo_salar.Ssal_v3.1.dna_sm.toplevel.fa'
-
 
 col_file="$PWD/data/col_files/${SPEC}_colnames.txt"
 
@@ -35,7 +35,7 @@ random_chars=$(printf "%s" "${chars:RANDOM%${#chars}:1}${chars:RANDOM%${#chars}:
 
 nextflow run main_susie.nf -resume \
     -w "work_dir_susie" \
-    -name "susie_${SPEC}_keras_${random_chars}" \
+    -name "susie_pig_keras_${random_chars}" \
     --col_file "$col_file" \
     --model "$model" \
     --vcfs "$PWD/data/susie_pig_gvf/*.txt" \
