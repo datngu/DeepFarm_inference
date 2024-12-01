a0d8fbb141e4b7a5b6cbe94452f0793221cb69d8
Dat T Nguyen <n.dat@outlook.com>
2024-12-01 12:15:50 +0100

    1st

--- run_pig_rexld.sh
+++ run_pig_rexld.sh
@@ -17,14 +17,14 @@ export NXF_SINGULARITY_CACHEDIR=/mnt/users/ngda/sofware/singularity
 export TOWER_ACCESS_TOKEN=eyJ0aWQiOiA3OTAxfS4xNGY5NTFmOWNiZmEwNjZhOGFkYzliZTg3MDc4YWI4ZTRiYTk4ZmI5
 
 
-SPEC='cattle'
+SPEC='pig'
 
 vcf_dir=/mnt/ScratchProjects/Aqua-Faang/dat_projects/vip_nf/data_vcf
 model_dir="/mnt/ScratchProjects/Aqua-Faang/dat_projects/dl_project/analysis_paper/selected_models"
 model="${model_dir}/${SPEC}_DanQ.h5"
 
-#genome='/mnt/users/ngda/genomes/pig/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa'
-genome='/mnt/users/ngda/genomes/cattle/Bos_taurus.ARS-UCD1.2.dna_sm.toplevel.fa'
+genome='/mnt/users/ngda/genomes/pig/Sus_scrofa.Sscrofa11.1.dna.toplevel.fa'
+#genome='/mnt/users/ngda/genomes/cattle/Bos_taurus.ARS-UCD1.2.dna_sm.toplevel.fa'
 #genome='/mnt/users/ngda/genomes/chicken/chromosomes_1_to_28_and_MT.fa'
 #genome='/mnt/users/ngda/genomes/atlantic_salmon/Salmo_salar.Ssal_v3.1.dna_sm.toplevel.fa'
 
@@ -38,6 +38,6 @@ nextflow run main_RExLD.nf -resume \
     -name "RExLD_${SPEC}_keras_${random_chars}" \
     --col_file "$col_file" \
     --model "$model" \
-    --vcfs "$vcf_dir/${SPEC}/chr25.vcf.gz" \
+    --vcfs "$vcf_dir/${SPEC}/chr17.vcf.gz" \
     --genome "$genome" \
     -with-tower
